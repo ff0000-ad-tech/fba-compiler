25c8db4fecb88e4a9637217db09e8b578287387d
Greg Connell <greg.connell@ff0000.com>
2018-01-04 12:14:15 -0800

    applies prettier settings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // IMPORTS
 const fs = require('fs')
 const fx = require('fs-extra') // https://github.com/jprichardson/node-fs-extra
-const crc32 = require('crc-32')	// https://github.com/SheetJS/js-crc32
+const crc32 = require('crc-32') // https://github.com/SheetJS/js-crc32
 const struct = require('python-struct') // https://github.com/danielgindi/node-python-struct
 const zlib = require('zlib')
 const constants = require('pngjs/lib/constants')
@@ -15,7 +15,6 @@ const debug = require('debug')
 const log = debug('fba-compiler')
 const log1 = debug('fba-compiler:+')
 debug.disable('fba-compiler:+')
-
 
 // -------------------------------------------------------------------------------------------------------------
 const AVG_BYTE_PER_PIXEL = 0.101879816436
@@ -33,7 +32,7 @@ let newChunks = []
 
 let args
 // -------------------------------------------------------------------------------------------------------------
-const compile = (_args) => {
+const compile = _args => {
 	args = _args
 	/* TODO: Document args:
 			{
@@ -44,8 +43,8 @@ const compile = (_args) => {
 				}]
 			}
 	 */
-	
-	// reset properties	
+
+	// reset properties
 	newChunks = []
 	// compileTypes = []
 	// if (compileImages)
@@ -53,18 +52,17 @@ const compile = (_args) => {
 
 	// if (compileFonts)
 	// 	compileTypes.push('fonts')
-	
+
 	totalAssets = 0
 	totalByteWeight = 0
 
 	// assets total - if none, abort
 	totalAssets = args.assets.length
 	log1('TOTAL ASSETS:', totalAssets)
-	if (totalAssets == 0)
-		return
+	if (totalAssets == 0) return
 
 	// prepare the chunks
-	return prepareNewChunks()	
+	return prepareNewChunks()
 }
 
 // -------------------------------------------------------------------------------------------------------------
@@ -75,29 +73,23 @@ const prepareNewChunks = () => {
 		// iterate assets
 		const promises = []
 		args.assets.forEach(asset => {
-			promises.push(
-				packAsset(
-					asset.chunkType, 
-					asset.path
-				)
-			)
+			promises.push(packAsset(asset.chunkType, asset.path))
 		})
 		Promise.all(promises)
-		.then(() => {
-			// faux image size		
-			determineImageDimensions()
+			.then(() => {
+				// faux image size
+				determineImageDimensions()
 
-			// build png
-			buildPng()
+				// build png
+				buildPng()
 
-			// resolve
-			resolve()
-		})
-		.catch(err => {
-			reject(err);
-		})		
+				// resolve
+				resolve()
+			})
+			.catch(err => {
+				reject(err)
+			})
 	})
-
 }
 
 // -------------------------------------------------------------------------------------------------------------
@@ -106,42 +98,43 @@ const packAsset = (chunkType, assetPath) => {
 			I couldn't find the raw buffer in the dependency graph.
 			url-loader seems to be automatically base64-ing the source
 	 */
-	return fx.readFile(assetPath)
-	.then((data) => {			
-		// required order:
-		// - fileNameSizeBA
-		// - fileNameBA
-		// - the asset byte array
+	return fx
+		.readFile(assetPath)
+		.then(data => {
+			// required order:
+			// - fileNameSizeBA
+			// - fileNameBA
+			// - the asset byte array
 
-		// get the file name 
-		const fileName = assetPath.substring(assetPath.lastIndexOf('/') + 1)
-		// file name as a byte array
-		const fileNameBA = Buffer.from(fileName)
-		// calculate size of the file name string
-		const fileNameSize = fileNameBA.length
-		// file name size as a byte array
-		const fileNameSizeBA = Buffer.from(struct.pack('!i',fileNameSize))
-		
-		// Full Asset: 
-		const fullAsset = Buffer.concat(
-			[fileNameSizeBA, fileNameBA, data], 
-			// If totalLength is not provided, it is calculated from the Buffer instances in list. 
-			// This however causes an additional loop to be executed in order to calculate the totalLength, 
-			// so it is faster to provide the length explicitly if it is already known.
-			fileNameSizeBA.length + fileNameBA.length + data.length
-		)
+			// get the file name
+			const fileName = assetPath.substring(assetPath.lastIndexOf('/') + 1)
+			// file name as a byte array
+			const fileNameBA = Buffer.from(fileName)
+			// calculate size of the file name string
+			const fileNameSize = fileNameBA.length
+			// file name size as a byte array
+			const fileNameSizeBA = Buffer.from(struct.pack('!i', fileNameSize))
 
-		log(` ${assetPath}`)
-		log1(' + chunk', chunkType, )
-		log1('    fileName:', fileName)
-		log1('    fileNameBA:', fileNameBA.length, '|', fileNameBA)
-		log1('    fileNameSize:', fileNameSize)
-		log1('    fileNameSizeBA:', fileNameSizeBA.length, '|', fileNameSizeBA)
-		log1('     - content_to_add:', fileName, data.length)
+			// Full Asset:
+			const fullAsset = Buffer.concat(
+				[fileNameSizeBA, fileNameBA, data],
+				// If totalLength is not provided, it is calculated from the Buffer instances in list.
+				// This however causes an additional loop to be executed in order to calculate the totalLength,
+				// so it is faster to provide the length explicitly if it is already known.
+				fileNameSizeBA.length + fileNameBA.length + data.length
+			)
 
-		packNewChunk(chunkType, fullAsset)
-	})
-	.catch(err => console.error(err))
+			log(` ${assetPath}`)
+			log1(' + chunk', chunkType)
+			log1('    fileName:', fileName)
+			log1('    fileNameBA:', fileNameBA.length, '|', fileNameBA)
+			log1('    fileNameSize:', fileNameSize)
+			log1('    fileNameSizeBA:', fileNameSizeBA.length, '|', fileNameSizeBA)
+			log1('     - content_to_add:', fileName, data.length)
+
+			packNewChunk(chunkType, fullAsset)
+		})
+		.catch(err => console.error(err))
 }
 
 // -------------------------------------------------------------------------------------------------------------
@@ -154,17 +147,14 @@ const packNewChunk = (chunkType, assetsBytes) => {
 
 	// increment total file weight by chunk size
 	totalByteWeight += chunkSize
-		
+
 	// Type
 	const chunkTypeBA = Buffer.from(chunkType)
 	// Content is assetsBytes
 
 	// Create a temporary byte array of the (Type + Content) for the CRC check.
 	const chunkBytesLength = chunkTypeBA.length + assetsBytes.length
-	const chunkBytes = Buffer.concat(
-		[chunkTypeBA, assetsBytes], 
-		chunkBytesLength
-	)
+	const chunkBytes = Buffer.concat([chunkTypeBA, assetsBytes], chunkBytesLength)
 	log1('\t\t chunk_bytes for CRC check:', chunkBytesLength)
 
 	// CRC
@@ -172,7 +162,7 @@ const packNewChunk = (chunkType, assetsBytes) => {
 	log1('\t\t\t crc:', crc)
 
 	// put them all together into a full chunk buffer
-		
+
 	// Size
 	const chunkSizeBA = Buffer.from(struct.pack('!i', chunkSize))
 	// CRC
@@ -184,12 +174,12 @@ const packNewChunk = (chunkType, assetsBytes) => {
 	// - Content
 	// - CRC
 	const fullChunkBA = Buffer.concat(
-		[chunkSizeBA, chunkTypeBA, assetsBytes, crcBA], 
+		[chunkSizeBA, chunkTypeBA, assetsBytes, crcBA],
 		chunkSizeBA.length + chunkTypeBA.length + assetsBytes.length + crcBA.length
 	)
 
-	log1('   Weight:', (chunkSize / 1024), 'kB (', chunkSize, 'bytes ), totalByteWeight:', totalByteWeight)
-	log1('   Size[4]:', (chunkSize / 1024), 'kB(', chunkSize, 'bytes )')
+	log1('   Weight:', chunkSize / 1024, 'kB (', chunkSize, 'bytes ), totalByteWeight:', totalByteWeight)
+	log1('   Size[4]:', chunkSize / 1024, 'kB(', chunkSize, 'bytes )')
 	log1('   Type[4]:', chunkType)
 	log1('   Content[' + String(chunkSize) + ']')
 	log1('   CRC[4]:', crc)
@@ -200,21 +190,18 @@ const packNewChunk = (chunkType, assetsBytes) => {
 }
 
 // -------------------------------------------------------------------------------------------------------------
-const packfbAc = () => {		
+const packfbAc = () => {
 	// Creates a Chunk for the total asset count
 	const chunkTypeBA = Buffer.from(chunkTypes['count'])
 	const chunkCountBA = Buffer.from(struct.pack('!i', totalAssets))
-	
+
 	log1('\t\t packfbAc(), self.totalAssets:', totalAssets)
 	log1('\t\t\t', chunkTypes['count'], '\tSize: 4 bytes')
 	log1('\t\t\t', chunkCountBA)
 	log1('\t\t\tCreate temp chunk to calculate CRC')
 
-	// Create a temporary byte array for the CRC check. 
-	const tmpBytes = Buffer.concat(
-		[chunkTypeBA, chunkCountBA], 
-		chunkTypeBA.length + chunkCountBA.length
-	)
+	// Create a temporary byte array for the CRC check.
+	const tmpBytes = Buffer.concat([chunkTypeBA, chunkCountBA], chunkTypeBA.length + chunkCountBA.length)
 	const crc = crc32.buf(tmpBytes)
 
 	// write in chunk
@@ -229,20 +216,17 @@ const packfbAc = () => {
 	// order:
 	// Size BA
 	// Chunk Type BA
-	// Content - Chunk Count BA 
+	// Content - Chunk Count BA
 	// CRC BA
 
-	return Buffer.concat(
-		[sizeBA, tmpBytes, crcBA],
-		sizeBA.length + tmpBytes.length + crcBA.length
-	)	
+	return Buffer.concat([sizeBA, tmpBytes, crcBA], sizeBA.length + tmpBytes.length + crcBA.length)
 }
 
 // -------------------------------------------------------------------------------------------------------------
 const determineImageDimensions = () => {
 	const totalEstPixels = totalByteWeight / AVG_BYTE_PER_PIXEL
 	imageDimension = Math.round(Math.sqrt(totalEstPixels))
-	
+
 	log1('New Chunks\n\ttotal bytes:', totalByteWeight)
 	log1('\t totalEstPixels:', totalEstPixels)
 	log1('\twidth/height:', imageDimension)
@@ -267,13 +251,9 @@ const buildPng = () => {
 
 	// fbAi and fbAf
 	chunks = chunks.concat(newChunks)
-	
+
 	// IDAT
-	var filteredData = packer.filterData(
-		new Buffer(4 * imageDimension * imageDimension), 
-		imageDimension, 
-		imageDimension
-	)
+	var filteredData = packer.filterData(new Buffer(4 * imageDimension * imageDimension), imageDimension, imageDimension)
 	// compress it
 	var compressedData = zlib.deflateSync(filteredData, packer.getDeflateOptions())
 	filteredData = null
@@ -292,11 +272,11 @@ const buildPng = () => {
 
 	// Emit
 	log(` ${args.target}`)
-	const targetPath = path.dirname(args.target);
+	const targetPath = path.dirname(args.target)
 	if (!fs.existsSync(targetPath)) {
-		mkdirp.sync(targetPath);
+		mkdirp.sync(targetPath)
 	}
-	fs.writeFile(args.target, pngBuffer, (error) => {  
+	fs.writeFile(args.target, pngBuffer, error => {
 		if (error) throw error
 	})
 
