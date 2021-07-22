e06f4d46a420ee75c8ba54acd31155168066def5
Greg Connell <gconnell@160over90.com>
2021-07-22 15:11:34 -0700

    support multiple payload output targets

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const types = require('./lib/types.js')
-const filter = require('./lib/filter.js')
 const compiler = require('./lib/compiler.js')
 
 module.exports = {
-	types,
-	filter,
 	compile: compiler.compile
 }
